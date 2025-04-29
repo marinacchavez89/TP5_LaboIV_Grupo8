ff8fe2d06de98d7d197ea5c790ab4ebae7d63575
Brenda <chainbren@gmail.com>
2025-04-29 20:38:16 -0300

    Menu Peliculas agregado

--- src/TP5/VentanaPrincipal.java
+++ src/TP5/VentanaPrincipal.java
@@ -1,13 +1,35 @@
 package TP5;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class VentanaPrincipal extends JFrame {
+	private JMenuBar JmenuBar;
+	private JMenu JMenuPeliculas;
+	private JMenuItem JMenuItemAgregar;
+	private JMenuItem JMenuItemListar;
+	
 	public VentanaPrincipal() {
         setTitle("Programa");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
         setLocationRelativeTo(null);
+        getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+        
+        JmenuBar = new JMenuBar();
+        setJMenuBar(JmenuBar);
+        
+        JMenuPeliculas = new JMenu("Peliculas");
+        JmenuBar.add(JMenuPeliculas);
+        
+        JMenuItemAgregar = new JMenuItem("Agregar");
+       
+        JMenuPeliculas.add(JMenuItemAgregar);
+                
+        JMenuItemListar = new JMenuItem("Listar");
+        JMenuPeliculas.add(JMenuItemListar);
     }
 }
